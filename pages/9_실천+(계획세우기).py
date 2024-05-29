376c7034c063a2d75491d02372fcd611fec095ee
kimjunhonumber <168261689+kimjunhonumber@users.noreply.github.com>
2024-05-30 00:29:10 +0900

    Update 9_실천+(계획세우기).py

--- pages/9_실천+(계획세우기).py
+++ pages/9_실천+(계획세우기).py
@@ -6,10 +6,13 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title="도덕성 활동 기록")
 
 # 제목
-st.title("도덕성 활동 기록")
+st.title("도덕적 행동 기록")
 
 # 데이터 입력 양식
-st.markdown("## 오늘의 도덕 활동을 기록하세요")
+st.markdown("## 내가 실천한 오늘의 도덕적 행동을 기록하세요.")
+
+# 사용자 이름 입력
+name = st.text_input("이름을 적으세요")
 
 # 데이터 입력
 date = st.date_input("날짜")
@@ -19,13 +22,16 @@ thought = st.text_area("느낀 점")
 
 # 입력 데이터를 저장할 데이터프레임 초기화
 if 'data' not in st.session_state:
-    st.session_state.data = pd.DataFrame(columns=['날짜', '덕목', '한 일', '느낀 점'])
+    st.session_state.data = pd.DataFrame(columns=['이름', '날짜', '덕목', '한 일', '느낀 점'])
 
 # 입력 버튼 클릭 시 데이터 저장
 if st.button("입력"):
-    new_data = pd.DataFrame({'날짜': [date], '덕목': [virtue], '한 일': [action], '느낀 점': [thought]})
-    st.session_state.data = pd.concat([st.session_state.data, new_data], ignore_index=True)
-    st.write("데이터가 저장되었습니다.")
+    if name and virtue and action and thought:
+        new_data = pd.DataFrame({'이름': [name], '날짜': [date], '덕목': [virtue], '한 일': [action], '느낀 점': [thought]})
+        st.session_state.data = pd.concat([st.session_state.data, new_data], ignore_index=True)
+        st.write("데이터가 저장되었습니다.")
+    else:
+        st.write("모든 필드를 입력해 주세요.")
 
 # 저장된 데이터 표시
 st.markdown("## 저장된 도덕 활동 기록")
@@ -42,5 +48,4 @@ if not st.session_state.data.empty:
     ax.set_ylabel("횟수")
 
     st.pyplot(fig)
-
 
