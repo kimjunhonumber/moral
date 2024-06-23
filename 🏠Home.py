516dabdb35e1f3657118dc02429e344156f2627f
kimjunhonumber <168261689+kimjunhonumber@users.noreply.github.com>
2024-06-24 08:22:56 +0900

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -49,17 +49,6 @@ st.markdown("""
 # 1행
 col1, col2, col3 = st.columns([1, 1, 1])
 
-with col1:
-    # 파란색 막대 안에 글을 넣는 HTML 코드
-html_code = """
-<div style="background-color:#4169E1; padding: 10px; border-radius: 10px; text-align:center; color: white; width: 100%;">
-    <h3 style="margin: 0;">생각AI</h3>
-</div>
-"""
-
-# HTML 코드를 Streamlit에 표시
-st.markdown(html_code, unsafe_allow_html=True)
-
     # st.markdown('<div style="background-color:#4169E1; padding: 10px; border-radius: 10px; text-align:center;">', unsafe_allow_html=True)
     # st.markdown('<h3 style="text-align:center;">생각AI</h3>', unsafe_allow_html=True)
     # img_html = image_to_html('images/생각이.png', 'https://2gaeyouhl8fwhqotlwa9uz.streamlit.app/%EC%83%9D%EA%B0%81AI')
