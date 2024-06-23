2d866cb5567b2b3df3d2fc00b8a2807b599caefd
kimjunhonumber <168261689+kimjunhonumber@users.noreply.github.com>
2024-06-24 08:36:53 +0900

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -35,9 +35,12 @@ with col2:
 
 # 애플리케이션 소개
 st.markdown("""
-    ## 🌟초등학생들의 인성교육을 위해 제작된 챗봇입니다. 
-    ## 🌟초등학생들의 인성교육을 위해 제작된 AI 챗봇입니다. 무엇을 도와 드릴까요? 
-""")
+    <div style="text-align: center;">
+        <span style="font-size: 24px; font-weight: bold;">
+            🌟초등학생들의 인성교육을 위해 제작된 AI 챗봇입니다. <br> 무엇을 도와 드릴까요?
+        </span>
+    </div>
+""", unsafe_allow_html=True)
 
 # 컬럼으로 레이아웃 구성
 # 1행
