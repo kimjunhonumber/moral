6fb201572d6366b90fd3035bf5178191b1d9a5be
kimjunhonumber <168261689+kimjunhonumber@users.noreply.github.com>
2024-06-24 09:00:28 +0900

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -79,8 +79,18 @@ with col2:
     st.markdown('<p style="text-align:center;"> 아는 힘, 생각하는 힘,    <br> 마음의 힘, 행동의 힘</p>', unsafe_allow_html=True)
     st.markdown('</div>', unsafe_allow_html=True)
 with col3:
-    st.markdown('<div style="background-color:#87CEEB; padding: 10px; border-radius: 10px; text-align:center;">', unsafe_allow_html=True)
-    st.markdown('<h3 style="text-align:center;">생각+</h3>', unsafe_allow_html=True)
+    # st.markdown('<div style="background-color:#87CEEB; padding: 10px; border-radius: 10px; text-align:center;">', unsafe_allow_html=True)
+    # st.markdown('<h3 style="text-align:center;">생각+</h3>', unsafe_allow_html=True)
+    # 파란색 막대 안에 글을 넣는 HTML 코드
+    html_code = """
+    <div style="background-color:#87CEEB; height: 30px; display: flex; align-items: center; justify-content: center; border-radius: 10px; text-align:center; color: white; width: 100%; display: inline-block;">
+        <h3 style="margin: 0; font-size: 18px;">생각AI</h3>
+    </div>
+    """
+    # HTML 코드를 Streamlit에 표시
+    st.markdown(html_code, unsafe_allow_html=True)
+
+    
     img_html = image_to_html('images/이미지생성.png', 'https://2gaeyouhl8fwhqotlwa9uz.streamlit.app/%EC%83%9D%EA%B0%81+(%EC%9D%B4%EB%AF%B8%EC%A7%80_%EC%83%9D%EC%84%B1)')
     st.markdown(img_html, unsafe_allow_html=True)
     st.markdown('<p style="text-align:center;">이야기에 어울리는  <br> 이미지 만들기 </span> </p>', unsafe_allow_html=True) 
