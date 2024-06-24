2270faa38031a5674a432b423f821fcefca2d2fe
kimjunhonumber <168261689+kimjunhonumber@users.noreply.github.com>
2024-06-24 10:53:54 +0900

    Update 1_생각AI.py

--- pages/1_생각AI.py
+++ pages/1_생각AI.py
@@ -61,8 +61,8 @@ with st.sidebar:
         thread = client.beta.threads.create()
         st.session_state.thread_id = thread.id
         st.subheader(f"Created Thread ID: {st.session_state.thread_id}")
-        st.info("스레드가 생성되었습니다.")
-        st.info("스레드 ID를 기억하면 대화내용을 이어갈 수 있습니다.")
+        st.info("ID가 생성되었습니다.")
+        st.info("ID를 기억하면 대화내용을 이어갈 수 있습니다.")
         st.divider()
         st.subheader("추천 질문")
         st.info("배려(덕목)의 뜻은?")
