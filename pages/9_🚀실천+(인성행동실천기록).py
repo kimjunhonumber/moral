00c932c122f99b5e57213069cff3ac1b11e2cb6d
kimjunhonumber <168261689+kimjunhonumber@users.noreply.github.com>
2024-07-01 20:58:55 +0900

    Update 9_🚀실천+(인성행동실천기록).py

--- pages/9_🚀실천+(인성행동실천기록).py
+++ pages/9_🚀실천+(인성행동실천기록).py
@@ -6,5 +6,5 @@ import matplotlib.pyplot as plt
 
 url ="https://docs.google.com/spreadsheets/d/1KksuDA2ZjkkNanlZXlw_t7iE7okYnI_UMxJZz8Lzl60/edit?gid=0#gid=0("gsheets", type=GSheetsConnection)
 
-data = conn.read(spreadsheet=url, uescols=list(range(2)))
+data = conn.read(spreadsheet=url, uescols=list(range(2)), worksheet="0#gid=0")
 st.dataframe(data)
