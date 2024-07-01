f4fd831970caa8acdeb5c2cadf281d5e5abc2233
kimjunhonumber <168261689+kimjunhonumber@users.noreply.github.com>
2024-07-01 20:53:28 +0900

    Update 9_🚀실천+(인성행동실천기록).py

--- pages/9_🚀실천+(인성행동실천기록).py
+++ pages/9_🚀실천+(인성행동실천기록).py
@@ -2,7 +2,6 @@ import streamlit as st
 from streamlit_gsheets impoort GSheetsConnection
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 
 url ="https://docs.google.com/spreadsheets/d/1KksuDA2ZjkkNanlZXlw_t7iE7okYnI_UMxJZz8Lzl60/edit?gid=0#gid=0("gsheets", type=GSheetsConnection)
