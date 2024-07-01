8944106ab7ab4d621167842b5d9c945fef484e58
kimjunhonumber <168261689+kimjunhonumber@users.noreply.github.com>
2024-07-01 20:49:57 +0900

    Update 9_🚀실천+(인성행동실천기록).py

--- pages/9_🚀실천+(인성행동실천기록).py
+++ pages/9_🚀실천+(인성행동실천기록).py
@@ -7,5 +7,5 @@ import seaborn as sns
 
 url ="https://docs.google.com/spreadsheets/d/1KksuDA2ZjkkNanlZXlw_t7iE7okYnI_UMxJZz8Lzl60/edit?gid=0#gid=0("gsheets", type=GSheetsConnection)
 
-date = conn.read(spreadsheet=url, uescols=[0,1])
-st.dataframe(date)
+data = conn.read(spreadsheet=url, uescols=[0,1])
+st.dataframe(data)
