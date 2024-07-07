526efccfb764953bc1bfdcbd36837ed953f76ba7
kimjunhonumber <168261689+kimjunhonumber@users.noreply.github.com>
2024-07-07 15:17:34 +0900

    Update 9_🚀실천+(인성행동실천기록).py

--- pages/9_🚀실천+(인성행동실천기록).py
+++ pages/9_🚀실천+(인성행동실천기록).py
@@ -1,6 +1,17 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
-import pandas as pd
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
+
+
+
 
 # Display Title and Description
 st.title("Vendor Management Portal")
