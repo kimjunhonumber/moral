e8319bc68768b8a154f331f0d8ea4be1c646e07f
kimjunhonumber <168261689+kimjunhonumber@users.noreply.github.com>
2024-07-09 09:28:04 +0900

    Update 9_🚀실천+(인성행동실천기록).py

--- pages/9_🚀실천+(인성행동실천기록).py
+++ pages/9_🚀실천+(인성행동실천기록).py
@@ -1,179 +1,117 @@
-# streamlit_app.py
+import calendar  # Core Python Module
+from datetime import datetime  # Core Python Module
 
-import streamlit as st
-from streamlit_gsheets import GSheetsConnection
+import plotly.graph_objects as go  # pip install plotly
+import streamlit as st  # pip install streamlit
+from streamlit_option_menu import option_menu  # pip install streamlit-option-menu
 
-# Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
+import database as db  # local import
 
-df = conn.read()
+# -------------- SETTINGS --------------
+incomes = ["Salary", "Blog", "Other Income"]
+expenses = ["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
+currency = "USD"
+page_title = "Income and Expense Tracker"
+page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+layout = "centered"
+# --------------------------------------
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
-# import streamlit as st
-# from streamlit_gsheets import GSheetsConnection
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title + " " + page_icon)
 
-# # Create a connection object.
-# conn = st.connection("gsheets", type=GSheetsConnection)
-# # streamlit_app.py
+# --- DROP DOWN VALUES FOR SELECTING THE PERIOD ---
+years = [datetime.today().year, datetime.today().year + 1]
+months = list(calendar.month_name[1:])
 
 
-# # Display Title and Description
-# st.title("Vendor Management Portal")
+# --- DATABASE INTERFACE ---
+def get_all_periods():
+    items = db.fetch_all_periods()
+    periods = [item["key"] for item in items]
+    return periods
 
-# # Constants
-# BUSINESS_TYPES = [
-#     "Manufacturer",
-#     "Distributor",
-#     "Wholesaler",
-#     "Retailer",
-#     "Service Provider",
-# ]
-# PRODUCTS = [
-#     "Electronics",
-#     "Apparel",
-#     "Groceries",
-#     "Software",
-#     "Other",
-# ]
 
-# # Establishing a Google Sheets connection
-# conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+# --- HIDE STREAMLIT STYLE ---
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
-# # Fetch existing vendors data
-# existing_data = conn.read(worksheet="Vendors", usecols=list(range(6)), ttl=5)
-# existing_data = existing_data.dropna(how="all")
+# --- NAVIGATION MENU ---
+selected = option_menu(
+    menu_title=None,
+    options=["Data Entry", "Data Visualization"],
+    icons=["pencil-fill", "bar-chart-fill"],  # https://icons.getbootstrap.com/
+    orientation="horizontal",
+)
 
-# action = st.selectbox(
-#     "Choose an Action",
-#     [
-#         "Onboard New Vendor",
-#         "Update Existing Vendor",
-#         "View All Vendors",
-#         "Delete Vendor",
-#     ],
-# )
+# --- INPUT & SAVE PERIODS ---
+if selected == "Data Entry":
+    st.header(f"Data Entry in {currency}")
+    with st.form("entry_form", clear_on_submit=True):
+        col1, col2 = st.columns(2)
+        col1.selectbox("Select Month:", months, key="month")
+        col2.selectbox("Select Year:", years, key="year")
 
-# if action == "Onboard New Vendor":
-#     st.markdown("Enter the details of the new vendor below.")
-#     with st.form(key="vendor_form"):
-#         company_name = st.text_input(label="Company Name*")
-#         business_type = st.selectbox(
-#             "Business Type*", options=BUSINESS_TYPES, index=None
-#         )
-#         products = st.multiselect("Products Offered", options=PRODUCTS)
-#         years_in_business = st.slider("Years in Business", 0, 50, 5)
-#         onboarding_date = st.date_input(label="Onboarding Date")
-#         additional_info = st.text_area(label="Additional Notes")
+        "---"
+        with st.expander("Income"):
+            for income in incomes:
+                st.number_input(f"{income}:", min_value=0, format="%i", step=10, key=income)
+        with st.expander("Expenses"):
+            for expense in expenses:
+                st.number_input(f"{expense}:", min_value=0, format="%i", step=10, key=expense)
+        with st.expander("Comment"):
+            comment = st.text_area("", placeholder="Enter a comment here ...")
 
-#         st.markdown("**required*")
-#         submit_button = st.form_submit_button(label="Submit Vendor Details")
+        "---"
+        submitted = st.form_submit_button("Save Data")
+        if submitted:
+            period = str(st.session_state["year"]) + "_" + str(st.session_state["month"])
+            incomes = {income: st.session_state[income] for income in incomes}
+            expenses = {expense: st.session_state[expense] for expense in expenses}
+            db.insert_period(period, incomes, expenses, comment)
+            st.success("Data saved!")
 
-#         if submit_button:
-#             if not company_name or not business_type:
-#                 st.warning("Ensure all mandatory fields are filled.")
-#             elif existing_data["CompanyName"].str.contains(company_name).any():
-#                 st.warning("A vendor with this company name already exists.")
-#             else:
-#                 vendor_data = pd.DataFrame(
-#                     [
-#                         {
-#                             "CompanyName": company_name,
-#                             "BusinessType": business_type,
-#                             "Products": ", ".join(products),
-#                             "YearsInBusiness": years_in_business,
-#                             "OnboardingDate": onboarding_date.strftime("%Y-%m-%d"),
-#                             "AdditionalInfo": additional_info,
-#                         }
-#                     ]
-#                 )
-#                 updated_df = pd.concat([existing_data, vendor_data], ignore_index=True)
-#                 conn.update(worksheet="Vendors", data=updated_df)
-#                 st.success("Vendor details successfully submitted!")
 
-# elif action == "Update Existing Vendor":
-#     st.markdown("Select a vendor and update their details.")
+# --- PLOT PERIODS ---
+if selected == "Data Visualization":
+    st.header("Data Visualization")
+    with st.form("saved_periods"):
+        period = st.selectbox("Select Period:", get_all_periods())
+        submitted = st.form_submit_button("Plot Period")
+        if submitted:
+            # Get data from database
+            period_data = db.get_period(period)
+            comment = period_data.get("comment")
+            expenses = period_data.get("expenses")
+            incomes = period_data.get("incomes")
 
-#     vendor_to_update = st.selectbox(
-#         "Select a Vendor to Update", options=existing_data["CompanyName"].tolist()
-#     )
-#     vendor_data = existing_data[existing_data["CompanyName"] == vendor_to_update].iloc[
-#         0
-#     ]
+            # Create metrics
+            total_income = sum(incomes.values())
+            total_expense = sum(expenses.values())
+            remaining_budget = total_income - total_expense
+            col1, col2, col3 = st.columns(3)
+            col1.metric("Total Income", f"{total_income} {currency}")
+            col2.metric("Total Expense", f"{total_expense} {currency}")
+            col3.metric("Remaining Budget", f"{remaining_budget} {currency}")
+            st.text(f"Comment: {comment}")
 
-#     with st.form(key="update_form"):
-#         company_name = st.text_input(
-#             label="Company Name*", value=vendor_data["CompanyName"]
-#         )
-#         business_type = st.selectbox(
-#             "Business Type*",
-#             options=BUSINESS_TYPES,
-#             index=BUSINESS_TYPES.index(vendor_data["BusinessType"]),
-#         )
-#         products = st.multiselect(
-#             "Products Offered",
-#             options=PRODUCTS,
-#             default=vendor_data["Products"].split(", "),
-#         )
-#         years_in_business = st.slider(
-#             "Years in Business", 0, 50, int(vendor_data["YearsInBusiness"])
-#         )
-#         onboarding_date = st.date_input(
-#             label="Onboarding Date", value=pd.to_datetime(vendor_data["OnboardingDate"])
-#         )
-#         additional_info = st.text_area(
-#             label="Additional Notes", value=vendor_data["AdditionalInfo"]
-#         )
+            # Create sankey chart
+            label = list(incomes.keys()) + ["Total Income"] + list(expenses.keys())
+            source = list(range(len(incomes))) + [len(incomes)] * len(expenses)
+            target = [len(incomes)] * len(incomes) + [label.index(expense) for expense in expenses.keys()]
+            value = list(incomes.values()) + list(expenses.values())
 
-#         st.markdown("**required*")
-#         update_button = st.form_submit_button(label="Update Vendor Details")
+            # Data to dict, dict to sankey
+            link = dict(source=source, target=target, value=value)
+            node = dict(label=label, pad=20, thickness=30, color="#E694FF")
+            data = go.Sankey(link=link, node=node)
 
-#         if update_button:
-#             if not company_name or not business_type:
-#                 st.warning("Ensure all mandatory fields are filled.")
-#             else:
-#                 # Removing old entry
-#                 existing_data.drop(
-#                     existing_data[
-#                         existing_data["CompanyName"] == vendor_to_update
-#                     ].index,
-#                     inplace=True,
-#                 )
-#                 # Creating updated data entry
-#                 updated_vendor_data = pd.DataFrame(
-#                     [
-#                         {
-#                             "CompanyName": company_name,
-#                             "BusinessType": business_type,
-#                             "Products": ", ".join(products),
-#                             "YearsInBusiness": years_in_business,
-#                             "OnboardingDate": onboarding_date.strftime("%Y-%m-%d"),
-#                             "AdditionalInfo": additional_info,
-#                         }
-#                     ]
-#                 )
-#                 # Adding updated data to the dataframe
-#                 updated_df = pd.concat(
-#                     [existing_data, updated_vendor_data], ignore_index=True
-#                 )
-#                 conn.update(worksheet="Vendors", data=updated_df)
-#                 st.success("Vendor details successfully updated!")
-
-# # View All Vendors
-# elif action == "View All Vendors":
-#     st.dataframe(existing_data)
-
-# # Delete Vendor
-# elif action == "Delete Vendor":
-#     vendor_to_delete = st.selectbox(
-#         "Select a Vendor to Delete", options=existing_data["CompanyName"].tolist()
-#     )
-
-#     if st.button("Delete"):
-#         existing_data.drop(
-#             existing_data[existing_data["CompanyName"] == vendor_to_delete].index,
-#             inplace=True,
-#         )
-#         conn.update(worksheet="Vendors", data=existing_data)
-#         st.success("Vendor successfully deleted!")
+            # Plot it!
+            fig = go.Figure(data)
+            fig.update_layout(margin=dict(l=0, r=0, t=5, b=5))
+            st.plotly_chart(fig, use_container_width=True)
